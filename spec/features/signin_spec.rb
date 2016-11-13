eda5cb6fd0105fb4df24b23fccc907ba4cf237d6
Nic Ollis <nic@ollis.me>
2016-11-12 19:33:21 -0500

    Creating Signin

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "signing in" do
+  let(:guest) {FactoryGirl.create(:guest)}
+  
+  def fill_in_signin_fields
+    fill_in "guest[email]", with: guest.email
+    fill_in "guest[password]", with: guest.password
+    click_button "Log in"
+  end
+  
+  scenario "visiting the site to sign in" do
+    visit root_path
+    click_link "Sign in"
+    fill_in_signin_fields
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
